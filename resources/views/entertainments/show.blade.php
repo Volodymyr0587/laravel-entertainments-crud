a24ca5734d301c49642f4cb5145a1623e3e50a64
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-11-08 20:02:44 +0200

    feat(ux): display tags on show entertainment page

--- resources/views/entertainments/show.blade.php
+++ resources/views/entertainments/show.blade.php
@@ -22,6 +22,14 @@
             </span>
         </div>
 
+        <div class="my-4">
+            @foreach ($entertainment->tags as $tag)
+                <span class="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 inset-ring inset-ring-purple-700/10">
+                {{ $tag->name }}
+                </span>
+            @endforeach
+        </div>
+
         <hr class="my-6">
 
         {{-- Details --}}
